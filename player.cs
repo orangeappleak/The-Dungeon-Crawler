a3daf53cef6adebc205e3d6a8d52fc061e03fafe
orangeappleak <40416975+orangeappleak@users.noreply.github.com>
2020-05-17 10:20:56 +0530

    Add files via upload
    some important unity scripts

---
+++ player.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    private Rigidbody2D _rigidBody;
+
+    private Sprite _sprite;
+
+    [SerializeField]
+    private bool _grounded = true;
+
+    [SerializeField]
+    private float _jumpForce = 5.0f;
+
+    [SerializeField]
+    private float _speed = 2.5f;
+
+    private bool reset_jump = false;
+
+    private playerAnimation _anim;
+
+    private SpriteRenderer _renderer;
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<playerAnimation>();
+        _renderer = GetComponentInChildren<SpriteRenderer>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        _grounded = _isOnGround();
+    }
+
+    void Movement(){
+
+        float move = Input.GetAxisRaw("Horizontal");
+        Debug.DrawRay(transform.position,Vector2.down,Color.red);
+        if(move<0) {
+            _renderer.flipX = true;
+        }
+        else if(move > 0) _renderer.flipX = false;
+
+        if(Input.GetKeyDown(KeyCode.Space) && _grounded){
+            _rigidBody.velocity = new Vector2(_rigidBody.velocity.x ,_jumpForce);
+            _anim.jumping(true);
+            StartCoroutine(JumpResetCoroutine());
+            
+        }
+        _rigidBody.velocity = new Vector2(move * _speed,_rigidBody.velocity.y);
+        _anim.Move(move);
+    }
+
+    bool _isOnGround(){
+        RaycastHit2D _hitGround = Physics2D .Raycast(transform.position,Vector2.down,1.2f, 1 << 8);
+        
+
+        if(_hitGround.collider!=null){
+            Debug.Log("hit:" + _hitGround.collider.name);
+            if(reset_jump == false){
+                _anim.jumping(false);
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    IEnumerator JumpResetCoroutine(){
+        reset_jump = true;
+        yield return new WaitForSeconds(0.5f);
+        reset_jump = false;
+    }
+
+}
---
+++ playerAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerAnimation : MonoBehaviour
+{
+
+    private Animator _anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    public void Move(float move){
+        _anim.SetFloat("Move", Mathf.Abs(move));
+    }
+
+    public void jumping(bool jump){
+        _anim.SetBool("jumping",jump);
+    }
+}
